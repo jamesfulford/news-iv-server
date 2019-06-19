7f380bf0cea04e015108a56f2152c02a68c92265
James Fulford <james.fulford@outlook.com>
2019-06-19 00:42:59 -0400

    feat(env): Using dotenv

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connection =
 	process.env.NEWSIV_MONGO_CONNECTION_STRING || "mongodb://localhost/news-iv";
 
-mongoose.set("debug", !process.env.NEWSIV_MONGO_CONNECTION_STRING);
+// If env is set
+mongoose.set("debug", !!process.env.MONGOOSE_DEBUG);
 mongoose.Promise = Promise;
 
 mongoose.connect(connection, {
