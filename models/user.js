51c7761f88b9a4414c41804fdf6888d92a49f99d
James Fulford <james.fulford@outlook.com>
2019-06-19 00:09:28 -0400

    WIP(db): Adding User model with password

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	profileImageUrl: {
+		type: String
+	}
+});
+
+userSchema.pre("save", async function(next) {
+	try {
+		if (!this.isModified("password")) {
+			return next();
+		}
+		const hashedPassword = await bcrypt.hash(this.password, 10);
+		this.password = hashedPassword;
+		return next();
+	} catch (e) {
+		return next(e);
+	}
+});
+
+userSchema.method.comparePassword = async function (candidatePassword, next) {
+	try {
+		return await bcrypt.compare(candidatePassword, this.password);
+	} catch (e) {
+		return next(e);
+	}
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { default: User };
