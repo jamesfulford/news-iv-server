b4c8ff9371b4b8d5c444881fffb9d7a471906ab5
James Fulford <james.fulford@outlook.com>
2019-06-19 00:42:14 -0400

    style(app): Lint

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(bodyParser.json());
 // Use routes here
 
 app.use(function(req, res, next) {
-  const err = new Error("Not found");
-  err.status = 404;
-  next(err);
+	const err = new Error("Not found");
+	err.status = 404;
+	next(err);
 });
-app.use(require('./handlers/error').default);
+
+app.use(require("./handlers/error").default);
 
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, console.log.bind(console, `Server started on port ${PORT}`));
