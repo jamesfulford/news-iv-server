b0cb634f46f11dfe69f54de4d85f2a1bda18efcd
James Fulford <james.fulford@outlook.com>
2019-06-19 00:08:58 -0400

    refactor(handlers): Moved error into handlers

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Use routes here
+
 app.use(function(req, res, next) {
   const err = new Error("Not found");
   err.status = 404;
   next(err);
 });
-app.use(require('./error').default);
+app.use(require('./handlers/error').default);
 
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, console.log.bind(console, `Server started on port ${PORT}`));
