2181f64d17332669f4cf4159da2ebf1e009a5657
James Fulford <james.fulford@outlook.com>
2019-06-23 23:31:30 -0400

    feat(app): Using env variable compatible with MongoDB plugin on Heroku

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connection =
-	process.env.NEWSIV_MONGO_CONNECTION_STRING || "mongodb://localhost/news-iv";
+	process.env.MONGODB_URI || "mongodb://localhost/news-iv";
 
 // If env is set
 mongoose.set("debug", !!process.env.MONGOOSE_DEBUG);
