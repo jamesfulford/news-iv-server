280ca3cbcdbcefabe9978c209efc555641cb804c
James Fulford <james.fulford@outlook.com>
2019-06-23 22:29:06 -0400

    fix(app): Fixing authorization issue

--- handlers/messages.js
+++ handlers/messages.js
@@ -29,8 +29,15 @@ exports.getMessage = async function (req, res, next) {
 exports.deleteMessage = async function (req, res, next) {
 	try {
 		const message = await db.Message.findById(req.params.messageId);
-		await message.remove();
-		return res.status(200).json(message);
+		if (message.user.equals(req.params.userId)) {
+			await message.remove();
+			return res.status(200).json(message);
+		}
+		return next({
+			status: 401,
+			message: "Unauthorized",
+			code: 9000
+		});
 	} catch (e) {
 		next(e);
 	}
